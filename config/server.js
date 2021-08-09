94fb414c4ffdc1b9a0ec612482ef6e53c83be67e
Nils Jacobsen <nils.jacobsen98@gmail.com>
2021-08-09 11:22:11 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'adf2026de7216f73b613bbb25d878661'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '13cf62c6-7c91-403b-94c3-2c2d3e5325c7'
+};
